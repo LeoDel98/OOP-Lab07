471c0958127785cd16d94c34613f6d67def60ce4
Leonardo Delvecchio <leonardo.delvecchio@studio.unibo.it>
2020-12-07 19:07:43 +0100

    Complete exercise Enum1

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -14,4 +14,24 @@ public enum Sport {
      * declare the following sports: - basket - soccer - tennis - bike - F1 -
      * motogp - volley
      */
+	
+	BASKET("Basket"),
+	
+	SOCCER("Soccer"),
+	
+	TENNIS("Tennis"),
+	
+	BIKE("Bike"),
+	
+	F1("F1"),
+	
+	MOTOGP("MotoGP"),
+	
+	VOLLEY("Volley");
+	
+	private final String sport;
+	
+	private Sport(final String sprt) {
+		this.sport = sprt;
+	}
 }
--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.nesting1.SportSocialNetworkUserImpl;
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -25,5 +28,31 @@ public final class TestSportByEnumeration {
          * By now, you know how to do it
          */
         // TODO
+    	
+    	final SportSocialNetworkUserImpl<User> dcassani = new SportSocialNetworkUserImpl<User>("Davide", "Cassani", "dcassani", 53);
+        final SportSocialNetworkUserImpl<User> becclestone = new SportSocialNetworkUserImpl<User>("Bernie", "Ecclestone", "becclestone", 83);;
+        final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<User>("Fernando", "Alonso", "falonso", 34);
+        
+        falonso.addSport(SportSocialNetworkUserImpl.F1);
+        falonso.addSport(SportSocialNetworkUserImpl.SOCCER);
+        falonso.addSport(SportSocialNetworkUserImpl.BIKE);
+        
+        System.out.println("Alonso practices F1: " + falonso.hasSport(SportSocialNetworkUserImpl.F1));
+        System.out.println("Alonso does not like volley: " + !falonso.hasSport(SportSocialNetworkUserImpl.VOLLEY));
+        
+        dcassani.addSport(SportSocialNetworkUserImpl.BIKE);
+        dcassani.addSport(SportSocialNetworkUserImpl.F1);
+        dcassani.addSport(SportSocialNetworkUserImpl.MOTOGP);
+        System.out.println("Cassani has been a professional biker: "
+                + dcassani.hasSport(SportSocialNetworkUserImpl.BIKE));
+        System.out.println("Cassani does not like soccer: " + !dcassani.hasSport(SportSocialNetworkUserImpl.SOCCER));
+        
+        becclestone.addSport(SportSocialNetworkUserImpl.F1);
+        becclestone.addSport(SportSocialNetworkUserImpl.BASKET);
+        System.out.println("Bernie's the boss when it comes to F1: "
+                + becclestone.hasSport(SportSocialNetworkUserImpl.F1));
+        System.out.println("Bernie does love playing also basket: "
+                + becclestone.hasSport(SportSocialNetworkUserImpl.BASKET));
+        
     }
 }
